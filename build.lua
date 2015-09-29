e7b5398faa2f66a628e4e8df2fbb8ee58a1dfedf
Bruno Le Floch <blflatex@gmail.com>
2015-09-29 20:36:50 +0200

    Run pdflatex one extra time to get all labels right

--- build.lua
+++ build.lua
@@ -3,5 +3,28 @@
 module = "gtl"
 checkengines = {"pdftex", "luatex", "xetex", "ptex"}
 
+typeset = function(file)
+  local errorlevel = tex(file)
+  if errorlevel == 0 then
+    local name = stripext(file)
+    errorlevel = biber(name) + bibtex(name)
+    if errorlevel == 0 then
+      local function cycle(name)
+        return(
+          makeindex(name, ".glo", ".gls", ".glg", glossarystyle) +
+          makeindex(name, ".idx", ".ind", ".ilg", indexstyle)    +
+          tex(file)                                              +
+          tex(file)
+        )
+      end
+      errorlevel = cycle(name)
+      if errorlevel ~= 0 then
+        errorlevel = cycle(name)
+      end
+    end
+  end
+  return errorlevel
+end
+
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))

f7722207c97189951e684fbcc2e891b8573c3f23
Bruno Le Floch <blflatex@gmail.com>
2015-09-29 20:19:42 +0200

    Test support of pTeX (needs a non-public version of l3build.lua)
    Actually my tests of plain TeX support for the last commit also
    need a non-public version of l3build.lua.

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env texlua
 -- Build script for gtl
 module = "gtl"
+checkengines = {"pdftex", "luatex", "xetex", "ptex"}
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
